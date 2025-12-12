24fa38b23a289b1d80480fdf29a24ab9e141a26a
AshleyN <ashwinidn0143@gmail.com>
2025-12-12 13:40:24 +0530

    commit to revert login change

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -59,7 +59,7 @@ public class LoginPageTest extends BaseTest {
 	public void doLoginTest(){
 		accountPage = loginPage.doLogin(prop.getProperty("userName"), prop.getProperty("password"));
 		String actualTitle = accountPage.accountPageTitle();
-		Assert.assertEquals(actualTitle, "My Account11");
+		Assert.assertEquals(actualTitle, "My Account");
 		log.info("Login page title is correct"+ actualTitle);
 	}
 
